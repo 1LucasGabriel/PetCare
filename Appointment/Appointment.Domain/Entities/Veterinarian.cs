eae0374b8dee5a07cd102528bef2cdae7d67419f
José Neto <jnetodesigns@gmail.com>
2026-05-27 20:15:32 -0300

    Criação de entidade Veterinarian

---
+++ Appointment/Appointment.Domain/Entities/Veterinarian.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Appointment.Domain.Entitiess
+{
+    public class Veterinarian
+    {
+        public Guid Id { get; private set; }
+        public string FullName { get; private set; }
+        public string Crmv { get; private set; }
+        public List<string>? Specialties { get; private set; }
+        public bool IsActive { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+    }
+}
