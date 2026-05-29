3c08f88ff2d8ec9d3a3bb64876e4667002e43639
DefinitelyNotVictor <DasMadhouse@outlook.com>
2026-05-29 17:52:07 -0300

    Refatoração dos métodos

--- Appointment/Appointment.Domain/Interfaces/IRepositories/IAppointmentRepository.cs
+++ Appointment/Appointment.Domain/Interfaces/IRepositories/IAppointmentRepository.cs
@@ -7,11 +7,13 @@ namespace Appointment.Domain.Interfaces.IRepositories
 {
     public interface IAppointmentRepository
     {
-        public void Create(Appointments appointment);
-        public void Update(Appointments appointment);
-        public void Delete(Guid Id);
-        public Appointments GetById(Guid id);
-        public List<Appointments> GetAll();
-
+        public Guid Create(Appointment appointment);
+        public Appointment? GetById(Guid id);
+        public List<Appointment> GetAll();
+        public List<Appointment> GetByVeterinarianId(Guid veterinarianId);
+        public List<Appointment> GetByPetId(Guid petId);
+        public void Update(Appointment appointment);
+        public void Delete(Guid id);
     }
 }
+
